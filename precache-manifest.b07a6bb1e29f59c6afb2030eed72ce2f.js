4ddce134d524be024b940faf97890e260315adda
Rathru3704 <chan0433@algonquinlive.com>
2020-05-25 12:16:25 -0400

    Updates

---
+++ precache-manifest.b07a6bb1e29f59c6afb2030eed72ce2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3781a2571950b55ead4a3792836b57f0",
+    "url": "/React_Express_PostgreSQL_cat-fe/index.html"
+  },
+  {
+    "revision": "d7e7ba236c6cd1d103b2",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "19c4c36b9373375437b2",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/css/main.105f2ce9.chunk.css"
+  },
+  {
+    "revision": "d7e7ba236c6cd1d103b2",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/js/2.bed51db8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/js/2.bed51db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c4c36b9373375437b2",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/js/main.22c99289.chunk.js"
+  },
+  {
+    "revision": "263214614353b101b90f",
+    "url": "/React_Express_PostgreSQL_cat-fe/static/js/runtime-main.26bef0a2.js"
+  }
+]);
